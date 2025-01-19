c2489c63434f1a88b8a1c3754ceebfbb88e79607
Lepp3 <lubaka05@gmail.com>
2025-01-20 00:05:10 +0200

    added home controller

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import homeController from './controllers/home-controller.js';
 
 const app = express();
 
@@ -11,9 +12,9 @@ app.set('view engine', 'hbs');
 app.set('views', './src/views');
 app.use('/static',express.static('src/public'));
 
-app.get('/', (req,res)=>{
-    res.render('home');
-});
+app.use(homeController);
+
+
 
 
 app.listen(5001, ()=> console.log('Server is listening on port http://localhost:5001'));
