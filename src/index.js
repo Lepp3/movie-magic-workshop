5822c16de34be456da32eec0d9e63fe42a4c74fd
Lepp3 <lubaka05@gmail.com>
2025-01-19 15:47:04 +0200

    set up all resources and public file structure

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ app.engine('hbs',handlebars.engine({
 
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
+app.use(express.static('src/public'));
 
 app.get('/', (req,res)=>{
     res.send('it works');
