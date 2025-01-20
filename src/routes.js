adc733fe1d91da962bb73733ce92f50e7f423190
Lepp3 <lubaka05@gmail.com>
2025-01-20 17:01:13 +0200

    implemented movie controller functionality

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import homeController from './controllers/home-controller.js';
+import movieController from './controllers/movie-controller.js';
 
 const routes = express.Router();
 
 routes.use(homeController);
+routes.use(movieController);
 
 routes.get('*', (req,res)=>{
     res.render('404');
