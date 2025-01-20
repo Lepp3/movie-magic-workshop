fc2d766a060c0bbe6528884399234dc8227494d7
Lepp3 <lubaka05@gmail.com>
2025-01-20 17:16:30 +0200

    added dynamic details link for each movie

--- src/movies.js
+++ src/movies.js
@@ -1,11 +1,13 @@
 export default [
     {
+        id: 1,
         name: 'Jungle Book',
         genre: 'comedy',
         description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
         imageUrl: '/static/img/jungle-cruise.jpeg'
     },
     {
+        id: 2,
         name: 'The Dark Knight',
         genre: 'Superhero',
         description: 'When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.',
