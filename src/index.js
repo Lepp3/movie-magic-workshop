025063aa3c13a06d0dc69537bebafe73c88101cf
Lepp3 <lubaka05@gmail.com>
2025-01-20 16:43:48 +0200

    implemented dynamic rendering on home page

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ app.engine('hbs',handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 app.use('/static',express.static('src/public'));
-
 app.use(routes);
 
 
--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,15 @@
-export default movies = [
+export default [
     {
         name: 'Jungle Book',
         genre: 'comedy',
         description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
-        imageUrl: './static/img/jungle-cruise.jpeg'
+        imageUrl: '/static/img/jungle-cruise.jpeg'
     },
     {
-        name: 'The Little Mermaid',
-        genre: 'Kids movie',
-        description: 'The youngest of King Triton\'s daughters, Ariel is a beautiful and spirited young mermaid with a thirst for adventure. Longing to find out more about the world beyond the sea, Ariel visits the surface and falls for the dashing Prince Eric. Following her heart, she makes a deal with the evil sea witch, Ursula, to experience life on land.',
-        imageUrl: './static/img/the-little-mermaid.jpg'
+        name: 'The Dark Knight',
+        genre: 'Superhero',
+        description: 'When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.',
+        imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UX1000_.jpg'
     }
 
 ]
