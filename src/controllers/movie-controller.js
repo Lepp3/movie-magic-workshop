376b855241800d3af6e95d9ac7032cb246ff47a3
Lepp3 <lubaka05@gmail.com>
2025-01-20 17:19:22 +0200

    added hardcoded movie details page

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -5,6 +5,10 @@ const movieController = Router();
 
 movieController.get('/create',(req,res)=>{
     res.render('create')
+});
+
+movieController.get('/:movieId/details',(req,res)=>{
+    res.render('details')
 })
 
 export default movieController;
