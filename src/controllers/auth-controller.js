05fe0b393e74f3c8a34e3ccd70b819ddf133cc3a
Lepp3 <lubaka05@gmail.com>
2025-02-02 18:17:09 +0200

    send data from register form

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,9 +1,15 @@
-import Router from 'express';
+import {Router} from 'express';
 
 const authController = Router();
 
 authController.get('/register', (req,res)=>{
-    res,render('auth/register')
+    res.render('auth/register');
+});
+
+authController.post('/register',async (req,res)=>{
+    const newUser = req.body;
+    console.log(newUser);
+    res.end();
 })
 
 
