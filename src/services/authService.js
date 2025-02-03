f843100301246af35d7c7fbe1d8bdad038ebfbf5
Lepp3 <lubaka05@gmail.com>
2025-02-03 11:19:42 +0200

    added login functionality with jwt generation

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,9 +1,33 @@
-import User from '../models/User.js'
+import User from '../models/User.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 
-
+const secret = process.env.SECRET;
 export default {
     register(userData){
         return User.create(userData);
+    },
+    async login(email,password){
+        let isValid = false;
+        const user = await User.findOne({email});
+
+        if(!user){
+            throw new Error('invalid email or password');
+        };
+
+        const isValidPassword = await bcrypt.compare(password,user.password);
+        if(!isValidPassword){
+            throw new Error('invalid email or password');
+        };
+        const payload = {
+            id: user._id,
+            email: user.email
+        }
+        const token = jwt.sign({payload},secret,{expiresIn: '2h'});
+
+        return token
+
     }
 }
